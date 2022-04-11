33da625ca808cdc4729347d425a8bdc3d7f1288f
lior shaked <Liorshak1986@gmail.com>
2022-04-11 03:54:26 +0300

    connected DB and seed inital data

--- components/PlayerLayout.tsx
+++ components/PlayerLayout.tsx
@@ -5,7 +5,7 @@ const PlayerLayout = ({children})=>{
 
     return (
         <Box width="100vw" height="100vh">
-<Box position="absolute" left="0" top="0" width ="250px">
+<Box position="absolute" left="0" top="0" width ="250px" >
     <SideBar />
 </Box>
 <Box marginLeft="250px" marginBottom="100px">{children}</Box> 
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,29 +1,118 @@
-import NextImage from "next/image"
+import NextImage from "next/image";
+import NextLink from "next/link";
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  // Center,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/layout";
 
 import {
-    Box,
-    List,
-    ListItem,
-    ListIcon,
-    Divider,
-    Center,
-    LinkBox,
-    LinkOverlay
-} from "@chakra-ui/layout"
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from "react-icons/md";
 
+const navMenu = [
+  {
+    name: "Home",
+    icon: MdHome,
+    route: "/",
+  },
+  {
+    name: "Search",
+    icon: MdSearch,
+    route: "/search",
+  },
+  {
+    name: "Your Library",
+    icon: MdLibraryMusic,
+    route: "/library",
+  },
+];
 
-const SideBar =()=>{
+const musicMenu = [
+  {
+    name: "Create Playlist",
+    icon: MdPlaylistAdd,
+    route: "/",
+  },
+  {
+    name: "Favorites",
+    icon: MdFavorite,
+    route: "/favorites",
+  },
+];
 
-    return (
-        <Box width="100%" height="calc(100vh - 100px)" bg="black" color="gray" paddingX="5px">
-            <Box paddingY="20px">
+const playLists = new Array(50).fill(1).map((_, i) => `PlayList ${i + 1}`);
 
-            <Box width="120px" paddingX="20px" marginBottom="20px">
-                <NextImage src="/logo.svg" width={120} height={60} />
-            </Box>
-
-            </Box>
+const SideBar = () => {
+  return (
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      color="gray"
+      paddingX="5px"
+    >
+      <Box paddingY="20px">
+        <Box width="120px" paddingX="20px" marginBottom="20px">
+          <NextImage src="/logo.svg" width={120} height={60} />
         </Box>
-    )
-}
-export default SideBar
+        <Box marginBottom="20px">
+          <List spacing={2}>
+            {navMenu.map((menu) => (
+              <ListItem paddingX="20px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay color="white">
+                      <ListIcon as={menu.icon} marginRight="20px" />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box marginBottom="20px">
+          <List spacing={2}>
+            {musicMenu.map((menu) => (
+              <ListItem paddingX="20px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay color="white">
+                      <ListIcon as={menu.icon} marginRight="20px" />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Divider marginLeft="20px" width="80%" color="gray.800" />
+      </Box>
+      <Box overflowY="auto" height="66%" paddingX="20px">
+        <List spacing={2}>
+          {playLists.map((playList) => (
+            <ListItem key={playList}>
+              <LinkBox>
+                <NextLink href="/">
+                  <LinkOverlay>{playList}</LinkOverlay>
+                </NextLink>
+              </LinkBox>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Box>
+  );
+};
+export default SideBar;
