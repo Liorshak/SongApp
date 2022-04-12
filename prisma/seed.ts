c5c60264b7567f6080271038dde1f166c68f5569
lior shaked <Liorshak1986@gmail.com>
2022-04-12 21:02:46 +0300

    signin - signup

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
+import bcrypt from "bcrypt"
 import { artistsData } from "./songsData";
 
 const prisma = new PrismaClient();
@@ -8,7 +8,7 @@ const run = async () => {
   await Promise.all(
     artistsData.map(async (artist) => {
       return prisma.artist.upsert({
-        where: {name:artist.name},
+        where: {name: artist.name},
         update:{},
         create:{
           name:artist.name,
@@ -24,7 +24,37 @@ const run = async () => {
         })
       })
         
-  )}
+  )
+
+const salt = bcrypt.genSaltSync()
+const user = await prisma.user.upsert({
+  where:{email:'user@test.com'},
+  update:{},
+  create:{
+    email: "user@test.com",
+    password: bcrypt.hashSync('something',salt),
+
+  },
+});
+
+const songs= await prisma.song.findMany({
+  
+})
+await Promise.all(new Array(10).fill(1).map(async (_,i)=>{
+  return prisma.playlist.create({
+    data:{
+      name:`Playlist ${i+1}`,
+      user:{
+        connect: {id:user.id}
+      },
+      songs:{
+        connect:songs.map((song)=>({
+        id:song.id,
+      }))}
+    }
+  })
+}))
+}
    
   
 
